8d83dc0dbc765435ef9a4a54210db31d2737c318
Quentin ROULON <quroulon@e2r7p6.42.fr>
2016-01-29 16:30:43 +0100

    ajouts

--- fdf.c
+++ fdf.c
@@ -50,6 +50,14 @@ void			ft_createpoint(t_point **pt, int x, int y, int alt)
 	}
 }
 
+int				ft_putkey(int keycode, void *param)
+{
+	ft_putstr("key event ");
+	ft_putnbrdl(keycode);
+	(void) param;
+	return (1);
+}
+
 int				main(int ac, char **av)
 {
 	void		*mlx;
@@ -60,6 +68,7 @@ int				main(int ac, char **av)
 	t_point		*pt1;
 	char		*str = NULL;
 	int			fd = 0;
+	double		a, b = 0;
 
 	if (ac == 2)
 	{
@@ -69,9 +78,11 @@ int				main(int ac, char **av)
 			x = ft_atoi(str);
 			while (ft_isdigit(*str))
 				str++;
+			str++;
 			y = ft_atoi(str);
 			while (ft_isdigit(*str))
 				str++;
+			str++;
 			alt = ft_atoi(str);
 			ft_createpoint(&pt1, x, y, alt);
 		}
@@ -85,41 +96,31 @@ int				main(int ac, char **av)
 		{
 			x = pt1->x;
 			y = pt1->y;
-			if (x > pt1->next->x)
-			{
-				while (x > pt1->next->x)
-				{
-					mlx_pixel_put(mlx, win, x, y, 0x00FF00FF);
-					x--;
-				}
-			}
-			else
+			a = (double) (pt1->next->y - pt1->y) / (pt1->next->x - pt1->x);
+			b = pt1->y - a * pt1->x;
+			if (x < pt1->next->x)
 			{
 				while (x < pt1->next->x)
 				{
-					mlx_pixel_put(mlx, win, x, y, 0x00FF00FF);
+					y = (int) (a * x + b);
+					mlx_pixel_put(mlx, win, x, y, 0x00FFFFFF); //blanc
 					x++;
 				}
 			}
-			if (y > pt1->next->y)
-			{
-				while (y > pt1->next->y)
-				{
-					mlx_pixel_put(mlx, win, x, y, 0x00FF00FF);
-					y--;
-				}	
-			}
 			else
 			{
-				while (y < pt1->next->y)
+				while (x > pt1->next->x)
 				{
-					mlx_pixel_put(mlx, win, x, y, 0x00FF00FF);
-					y++;
-				}
+					y = (int) (a * x + b);
+					mlx_pixel_put(mlx, win, x, y, 0x0000FFFF); //blanc
+					x--;
+				}	
 			}
+
 			pt1 = pt1->next;
 		}
 		// ft_trace();
+		mlx_key_hook(win, ft_putkey, 0);
 		mlx_loop(mlx);
 	}
 	return (1);
