3b2cf1358d17a8dcd04eee2c7ebd278faf11157a
Quentin ROULON <quroulon@e2r2p3.42.fr>
2016-03-15 14:17:35 +0100

    ajout minilibx en dur

--- Makefile
+++ Makefile
@@ -13,11 +13,12 @@
 NAME 		= 	fdf
 
 CC			= 	gcc
-CFLAGS		= 	-Wall -Werror -Wextra -g
+CFLAGS		= 	-Wall -Werror -Wextra
 
 SRC 		= 	fdf.c creation.c utilities.c trace.c menu.c
 
-LIB			=	libft/libft.a -lmlx -framework OpenGL -framework AppKit
+LIB			=	-Llibft -lft -Lminilibx_macos -lmlx -framework OpenGL \
+				-framework AppKit
 
 OBJ			=	$(SRC:.c=.o)
 
@@ -29,18 +30,21 @@ all: $(NAME)
 .SILENT : $(NAME)
 $(NAME):
 	# cd libft/ && $(MAKE) re
+	# cd minilibx_macos/ && $(MAKE) re
 	$(CC) $(CFLAGS) -c $(HEADER) $(SRC)
 	$(CC) -o $(NAME) $(OBJ) $(LIB)
 
 .SILENT : clean
 clean:
 	# cd libft/ && $(MAKE) clean
+	# cd minilibx_macos/ && $(MAKE) clean
 	rm -f $(OBJ)
 	rm -f *.gch
 
 .SILENT : fclean
 fclean: clean
 	# cd libft/ && $(MAKE) fclean
+	# cd minilibx_macos/ && $(MAKE) clean
 	rm -f $(NAME)
 
 re: fclean all
--- fdf.c
+++ fdf.c
@@ -12,24 +12,6 @@
 
 #include "fdf.h"
 
-static void		ft_putpoints(t_point *pnt)
-{
-	while (pnt->prev)
-		pnt = pnt->prev;
-	while (pnt)
-	{
-		ft_putnbr(pnt->x);
-		ft_putstr(" - ");
-		ft_putnbr(pnt->y);
-		ft_putstr(" - ");
-		ft_putnbr(pnt->alt);
-		ft_putstr(" - ");
-		ft_putnbr(pnt->color);
-		ft_putstr(";\n");
-		pnt = pnt->next;
-	}
-}
-
 int				ft_putkey(int keycode, t_tool *tool)
 {	
 	// ft_putstr("key event ");
@@ -76,33 +58,19 @@ int				main(int ac, char **av)
 
 	help = NULL;
 	tool = NULL;
-	ft_putendl("1");
 	ft_help1(&help);
-	ft_putendl("2");
 	pnt = ft_read_file(NULL, help, av[1], NULL);
-	ft_putendl("FIN");
-	ft_putpoints(pnt);
-	mlx = mlx_init();
-	// if ((mlx = mlx_init()) == NULL)
-	// {
-	// 	ft_putendl("mlx_init marche pas");
-	// 	ft_error();
-	// }
-	ft_putendl("Yop or not");
-	if ((win = mlx_new_window(mlx, LENGTH, LENGTH, "Test_point")) == NULL)
-	{
-		ft_putendl("mlx_new_window marche pas");
+	ft_putendl("JOULI GANT");
+	if ((mlx = mlx_init()) == NULL)
 		ft_error();
-	}
-	ft_putendl("3");
+	ft_putendl("OUPA");
+	if ((win = mlx_new_window(mlx, LENGTH, LENGTH, "Test_point")) == NULL)
+		ft_error();
 	ft_help2(pnt, &tool, mlx, win);
-	ft_putendl("4");
 	ft_atthebeginning(&pnt);
 	tool->p = pnt;
-	ft_putendl("5");
 	ft_menu(tool);
 	// ft_trace(tool);
-	ft_putendl("6");
 	mlx_key_hook(tool->win, ft_putkey, tool);
 	// mlx_loop_hook(tool->win, ft_putyolo, tool);
 	mlx_loop(tool->mlx);
